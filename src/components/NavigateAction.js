1b9a531c6df6168d76135bd9dbb2c62d3c94527d
Ed <lookatemail@gmail.com>
2021-07-07 11:19:22 +0100

    Refactor naviagtion action component

--- src/components/NavigateAction.js
+++ src/components/NavigateAction.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, Image, Dimensions} from 'react-native';
 
 import Colors from '../config/colors';
 import LeftIcon from '../assets/icons/back-left.png';
+import CustomButton from './CustomButton';
+const {width} = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
   container: {
@@ -12,11 +14,11 @@ const styles = StyleSheet.create({
   },
   text: {
     color: Colors.white,
-    textAlign: 'center',
+    textAlign: 'right',
     fontFamily: 'Sailec',
     fontSize: 16,
+    paddingRight: width / 3,
   },
-  iconContainer: {},
   textContainer: {
     flex: 1,
   },
@@ -25,9 +27,11 @@ const styles = StyleSheet.create({
 const NavigateAction = ({title, onPress}) => {
   return (
     <View style={styles.container}>
-      <TouchableOpacity style={styles.iconContainer} onPress={onPress}>
-        <Image source={LeftIcon} />
-      </TouchableOpacity>
+      <CustomButton
+        onPress={onPress}
+        image={<Image source={LeftIcon} />}
+        style={{backgroundColor: Colors.transparent}}
+      />
       <View style={styles.textContainer}>
         <Text style={styles.text}>{title}</Text>
       </View>
