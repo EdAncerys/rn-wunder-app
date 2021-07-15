a16e6cf715e13dcc0eb7f14846e32335726fa2b9
Ed <lookatemail@gmail.com>
2021-07-15 13:18:59 +0100

    Refactor - extract flatlist component to server function

--- src/screens/donate/DonationNotification.js
+++ src/screens/donate/DonationNotification.js
@@ -7,6 +7,7 @@ import {
   StatusBar,
   ScrollView,
   FlatList,
+  ImageBackground,
 } from 'react-native';
 
 import Colors from '../../config/colors';
@@ -15,8 +16,15 @@ import CustomButton from '../../components/CustomButton';
 import LeftBlack from '../../assets/icons/app/left-black.png';
 import ProfileBeth from '../../assets/icons/content/profile-beth.png';
 import NotificationsImgOne from '../../assets/icons/content/notification-img-one.png';
+import ProfileSarah from '../../assets/icons/content/profile-sarah.png';
+import NotificationsImgTwo from '../../assets/icons/content/notification-img-two.png';
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
   navigationContainer: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -39,22 +47,36 @@ const styles = StyleSheet.create({
     paddingHorizontal: '8%',
     marginVertical: '2%',
   },
-  profileWrapper: {
+  notificationContainer: {
     flexDirection: 'row',
-    marginVertical: '5%',
+    marginVertical: '3%',
   },
   avatarContainer: {
-    paddingRight: 10,
+    marginRight: 10,
   },
-  notificationContainer: {
+  msgContainer: {
     flex: 1,
   },
-  notificationImgContainer: {
+  imgContainerBackground: {
+    width: 32,
+    height: 40,
+    borderRadius: 5,
+    marginLeft: 10,
+    overflow: 'hidden',
+  },
+  imgWrapper: {
+    flex: 1,
     backgroundColor: Colors.screenFilter,
-    paddingLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  msgCoins: {
+    ...Fonts.N_400_14,
+    color: Colors.white,
   },
   title: {
     ...Fonts.N_500_14,
+    marginVertical: '3.5%',
   },
   name: {
     ...Fonts.N_400_14,
@@ -79,10 +101,10 @@ const DATA_MOST_RECENT = [
     name: 'Sarah Alderidge',
     createdAt: 'Nov 17th, 2020',
     notificationMsg: 'Donated 5 coins to: Daily meditation for a...',
-    avatar: ProfileBeth,
-    notificationImg: NotificationsImgOne,
+    avatar: ProfileSarah,
+    notificationImg: NotificationsImgTwo,
     action: 'donate',
-    coins: '30',
+    coins: '5',
   },
 ];
 
@@ -90,29 +112,31 @@ const DATA_MOST_RECENT = [
 const renderMostRecent = ({item, index}) => {
   return (
     <ScrollView>
-      <View style={styles.profileWrapper}>
+      <View style={styles.notificationContainer}>
         <View style={styles.avatarContainer}>
           <Image source={item.avatar} />
         </View>
-        <View style={styles.notificationContainer}>
+        <View style={styles.msgContainer}>
           <Text style={styles.name}>{item.name}</Text>
           <Text style={styles.notificationMsg}>{item.notificationMsg}</Text>
         </View>
-        <View style={styles.notificationImgContainer}>
-          <Image source={item.notificationImg} />
+        <View style={styles.imgContainer}>
+          <ImageBackground
+            source={item.notificationImg}
+            style={styles.imgContainerBackground}>
+            <View style={styles.imgWrapper}>
+              <Text style={styles.msgCoins}>{item.coins}</Text>
+            </View>
+          </ImageBackground>
         </View>
       </View>
     </ScrollView>
   );
 };
 
-// RETURN ---------------------------------------------------------
-const DonationNotification = ({navigation}) => {
-  const [dataMostRecent, setDataMostRecent] = React.useState(DATA_MOST_RECENT);
-
+const ServeNavigation = ({props}) => {
   return (
     <View>
-      <StatusBar hidden />
       <View style={styles.navigationContainer}>
         <CustomButton
           image={LeftBlack}
@@ -125,23 +149,33 @@ const DonationNotification = ({navigation}) => {
         </View>
       </View>
       <View style={styles.divider}></View>
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>most recent</Text>
-        <FlatList
-          data={dataMostRecent}
-          keyExtractor={(_, index) => String(index)}
-          renderItem={renderMostRecent}
-        />
-        <View style={styles.divider}></View>
-      </View>
+    </View>
+  );
+};
 
-      <View style={styles.contentContainer}>
-        <Text style={styles.title}>this week</Text>
-        <ScrollView>
-          <Text>Content</Text>
-        </ScrollView>
-        <View style={styles.divider}></View>
-      </View>
+const ServeData = ({data, title}) => {
+  return (
+    <View style={styles.contentContainer}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        data={data}
+        keyExtractor={(_, index) => String(index)}
+        renderItem={renderMostRecent}
+      />
+      <View style={styles.divider}></View>
+    </View>
+  );
+};
+
+// RETURN ---------------------------------------------------------
+const DonationNotification = ({navigation}) => {
+  const [dataMostRecent, setDataMostRecent] = React.useState(DATA_MOST_RECENT);
+
+  return (
+    <View>
+      <StatusBar hidden />
+      <ServeNavigation />
+      <ServeData data={dataMostRecent} title="most recent" />
     </View>
   );
 };
