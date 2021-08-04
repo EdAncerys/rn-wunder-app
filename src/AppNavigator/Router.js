b343bb25d54fedfd672112c3d83384a434f3b874
Ed <lookatemail@gmail.com>
2021-08-04 11:41:38 +0100

    Refactor Profile screen - add drag handler with flatList

--- src/AppNavigator/Router.js
+++ src/AppNavigator/Router.js
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {View} from 'react-native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {openCameraPopUp} from '../config/deviceCamera';
 
 import {useAuthState, useAuthDispatch, addPostAction} from '../context/auth';
 import {useApiDispatch} from '../context/api';
@@ -75,7 +74,7 @@ export const AppNavigator = () => {
     <Stack.Navigator initialRouteName="CreateAccountStack">
       {/* <Stack.Screen
         name="TestScreen"
-        component={Draft}
+        component={Profile}
         options={{headerShown: false}}
       /> */}
       {/* <Stack.Screen
--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -8,11 +8,13 @@ import {
   FlatList,
   Image,
   ScrollView,
+  SafeAreaView,
+  StatusBar,
+  ImageBackground,
 } from 'react-native';
 import {useAuthState} from '../../context/auth';
 import AddPostAction from '../../components/AddPostAction';
 import {PROFILE_DATA, IMAGE_DATA_ARRAY} from '../../config/data';
-import Draggable from 'react-native-draggable';
 import SlidingUpPanel from 'rn-sliding-up-panel';
 
 import ScreenWrapper from '../../components/ScreenWrapper';
@@ -30,19 +32,22 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   headerActions: {
+    zIndex: 2,
+    position: 'absolute',
     justifyContent: 'center',
     marginHorizontal: '5%',
-    marginVertical: '5%',
+    marginVertical: '15%',
+    // backgroundColor: 'pink',
   },
   appActions: {
-    flex: 1.5,
+    marginTop: height / 3,
     alignItems: 'flex-start',
     justifyContent: 'center',
     marginHorizontal: '5%',
-    backgroundColor: Colors.matFilter,
   },
   postContainer: {
     flex: 1,
+    marginTop: height / 8,
     marginHorizontal: '5%',
   },
   rowWrapper: {
@@ -59,13 +64,7 @@ const styles = StyleSheet.create({
     width: width * 0.5,
   },
   flatListContainer: {
-    // zIndex: 1,
-    // position: 'absolute',
-    // marginTop: height - 350,
     backgroundColor: Colors.white,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    overflow: 'hidden',
   },
   image: {
     height: height / 4 + 50,
@@ -80,6 +79,7 @@ const Profile = ({navigation}) => {
   const [profile, setProfile] = React.useState(PROFILE_DATA);
   const [projects, setProjects] = React.useState(IMAGE_DATA_ARRAY);
   const {name, followers, about} = profile;
+  const {url} = profile;
 
   React.useEffect(() => {
     if (addAction) setAddPostPopUp(addAction.addAction);
@@ -158,58 +158,74 @@ const Profile = ({navigation}) => {
 
   // RETURN ---------------------------------------------------------
   return (
-    <ScreenWrapper image={Background}>
-      {addPostPopUp && <AddPostAction navigation={navigation} />}
-      <View style={styles.wrapper}>
+    <ImageBackground
+      source={url}
+      style={{
+        flex: 1,
+        resizeMode: 'cover',
+        justifyContent: 'center',
+      }}>
+      <StatusBar hidden />
+      <SafeAreaView style={styles.wrapper}>
+        {addPostPopUp && <AddPostAction navigation={navigation} />}
         <View style={styles.headerActions}>
           <DonateActions navigation={navigation} profile />
         </View>
+        <View style={styles.appActions}>
+          <AppActions
+            navigation={navigation}
+            Settings
+            Shoutout
+            item={profile}
+          />
+        </View>
+        <View style={styles.postContainer}>
+          <ServeProfileInfo />
+        </View>
 
-        <View style={{flex: 1}}>
-          <View style={styles.appActions}>
-            <AppActions
-              navigation={navigation}
-              Settings
-              Shoutout
-              item={profile}
-            />
-          </View>
-          <View style={styles.postContainer}>
-            <ServeProfileInfo />
-          </View>
-
-          <SlidingUpPanel
-            backdropOpacity={1}
-            draggableRange={{top: height, bottom: 300}}
-            // draggableRange={{top: height, bottom: 200}}
-          >
-            {/* <ScrollView
-              style={{flexGrow: 1}}
-              showsVerticalScrollIndicator={false}
-              horizontal={false}> */}
-            <View style={styles.flatListContainer}>
-              {/* <FlatList
-                keyExtractor={(_, index) => String(index)}
-                showsVerticalScrollIndicator={false}
-                numColumns={3}
-                data={projects}
-                renderItem={renderFlatListItem}
-                nestedScrollEnabled={true}
-              /> */}
+        <SlidingUpPanel
+          snappingPoints={[height / 2.1, height]}
+          draggableRange={{top: height - height / 7, bottom: 220}}>
+          {dragHandler => (
+            <View
+              style={{
+                flex: 1,
+                zIndex: 1,
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginBottom: 230,
+              }}>
               <View
                 style={{
-                  minHeight: height,
-                  width: width / 2,
-                  backgroundColor: 'pink',
-                }}>
-                <Text>Helllo</Text>
+                  alignSelf: 'stretch',
+                  height: 64,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  backgroundColor: Colors.matFilter,
+                  borderTopLeftRadius: 30,
+                  borderTopRightRadius: 30,
+                  overflow: 'hidden',
+                }}
+                {...dragHandler}>
+                <Text>Drag handler</Text>
               </View>
+              <ScrollView>
+                <View style={styles.flatListContainer}>
+                  <FlatList
+                    keyExtractor={(_, index) => String(index)}
+                    showsVerticalScrollIndicator={false}
+                    numColumns={3}
+                    data={projects}
+                    renderItem={renderFlatListItem}
+                    nestedScrollEnabled={true}
+                  />
+                </View>
+              </ScrollView>
             </View>
-            {/* </ScrollView> */}
-          </SlidingUpPanel>
-        </View>
-      </View>
-    </ScreenWrapper>
+          )}
+        </SlidingUpPanel>
+      </SafeAreaView>
+    </ImageBackground>
   );
 };
 
